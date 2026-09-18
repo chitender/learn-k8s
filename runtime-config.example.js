a604e97681fa26ce7cefb58c8319c4efac8bc6fd
Chitender Kumar <chitenderkumar.16@gmail.com>
2026-09-18 14:56:26 +0530

    security: remove Razorpay key from runtime-config.example.js

--- runtime-config.example.js
+++ runtime-config.example.js
@@ -3,7 +3,6 @@ window.LEARN_K8S_CONFIG = Object.freeze({
   awsRegion: "ap-south-1",
   cognitoClientId: "",
   cognitoUserPoolId: "",
-  razorpayKeyId: "",
   paidExamsEnabled: false,
   liveLabsEnabled: false
 });
