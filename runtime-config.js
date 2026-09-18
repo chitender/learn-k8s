6f523289c990d8c774ef535723f973c92d1baa1d
Chitender Kumar <chitenderkumar.16@gmail.com>
2026-09-18 14:23:31 +0530

    chore: add runtime-config.js fallback

---
+++ runtime-config.js
@@ -0,0 +1,9 @@
+window.LEARN_K8S_CONFIG = Object.freeze({
+  apiUrl: "",
+  awsRegion: "ap-south-1",
+  cognitoClientId: "",
+  cognitoUserPoolId: "",
+  razorpayKeyId: "",
+  paidExamsEnabled: false,
+  liveLabsEnabled: false
+});
