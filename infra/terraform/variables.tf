255c21b6611655e63a1e3db6906b37d77b8ed055
Chitender Kumar <chitenderkumar.16@gmail.com>
2026-09-18 14:26:36 +0530

    fix: format infra/terraform/variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,1 +1,40 @@
-variable "aws_region" { type = string default = "ap-south-1" }\nvariable "site_name" { type = string default = "learn-k8s" }\nvariable "razorpay_key_id" { type = string default = "" sensitive = true }\nvariable "paid_exams_enabled" { type = bool default = false }\nvariable "enable_live_labs" { type = bool default = false }\nvariable "ckad_price_inr" { type = number default = 999 }\nvariable "cks_price_inr" { type = number default = 1299 }\nvariable "lab_instance_type" { type = string default = "t3a.medium" }\n
+variable "aws_region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "site_name" {
+  type    = string
+  default = "learn-k8s"
+}
+
+variable "razorpay_key_id" {
+  type      = string
+  default   = ""
+  sensitive = true
+}
+
+variable "paid_exams_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "enable_live_labs" {
+  type    = bool
+  default = false
+}
+
+variable "ckad_price_inr" {
+  type    = number
+  default = 999
+}
+
+variable "cks_price_inr" {
+  type    = number
+  default = 1299
+}
+
+variable "lab_instance_type" {
+  type    = string
+  default = "t3a.medium"
+}
