df89560d4df20831bfda198f6392c47c29f0c2f1
Chitender Kumar <chitenderkumar.16@gmail.com>
2026-09-18 14:12:30 +0530

    feat: add runtime-config.example.js

---
+++ runtime-config.example.js
@@ -0,0 +1,9 @@
+window.LEARN_K8S_CONFIG = Object.freeze({
+  apiUrl: "https://example.execute-api.ap-south-1.amazonaws.com",
+  awsRegion: "ap-south-1",
+  cognitoClientId: "",
+  cognitoUserPoolId: "",
+  razorpayKeyId: "",
+  paidExamsEnabled: false,
+  liveLabsEnabled: false
+});
