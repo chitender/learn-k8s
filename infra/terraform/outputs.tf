1963720e1fa04ef3bf9fe0644f69340a3bcfe343
Chitender Kumar <chitenderkumar.16@gmail.com>
2026-09-18 14:26:39 +0530

    fix: format infra/terraform/outputs.tf

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,1 +1,31 @@
-output "site_bucket" { value = aws_s3_bucket.site.id }\noutput "site_url" { value = "https://${aws_cloudfront_distribution.site.domain_name}" }\noutput "cloudfront_distribution_id" { value = aws_cloudfront_distribution.site.id }\noutput "api_url" { value = aws_apigatewayv2_api.api.api_endpoint }\noutput "cognito_user_pool_id" { value = aws_cognito_user_pool.users.id }\noutput "cognito_client_id" { value = aws_cognito_user_pool_client.web.id }\noutput "razorpay_secret_arn" { value = aws_secretsmanager_secret.razorpay.arn }\noutput "lab_url" { value = var.enable_live_labs ? "https://${aws_cloudfront_distribution.labs[0].domain_name}" : "" }\n
+output "site_bucket" {
+  value = aws_s3_bucket.site.id
+}
+
+output "site_url" {
+  value = "https://${aws_cloudfront_distribution.site.domain_name}"
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.site.id
+}
+
+output "api_url" {
+  value = aws_apigatewayv2_api.api.api_endpoint
+}
+
+output "cognito_user_pool_id" {
+  value = aws_cognito_user_pool.users.id
+}
+
+output "cognito_client_id" {
+  value = aws_cognito_user_pool_client.web.id
+}
+
+output "razorpay_secret_arn" {
+  value = aws_secretsmanager_secret.razorpay.arn
+}
+
+output "lab_url" {
+  value = var.enable_live_labs ? "https://${aws_cloudfront_distribution.labs[0].domain_name}" : ""
+}
